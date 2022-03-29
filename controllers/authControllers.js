242ffeb1adb6dcc5883c7da957b0c07fdfba56ca
Arvind M <arvind.nexus06@gmail.com>
2022-03-29 11:27:41 +0530

    user registartion api initialization

---
+++ controllers/authControllers.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+
+const jwt = require("jsonwebtoken");
+
+// web token
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "user registration api", {
+    expiresIn: maxAge,
+  });
+};
+
+module.exports.signup_post = async (req, res) => {
+  console.log("calling");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+
+    console.log({ user });
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: { id: user._id, email, token } });
+  } catch (err) {
+    // const errors = handleErrors(err);
+    res.status(400).json(err);
+  }
+};
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+    res.status(200).json({ user: { id: user._id, email, token } });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+const PORT = process.env.APP_PORT || 8000;
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.DB_STRTING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log("server is running on " + PORT));
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("User Registration Express API");
+});
+
+app.use(authRoutes);
+
+// app.listen(PORT);
